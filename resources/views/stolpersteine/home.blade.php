ba735b74782453d9190f24bb2ec6b790a4c44e9a
JuanGT01 <jgato@iesflorenciopintado.es>
2022-05-30 21:49:21 +0200

    Map fix

--- resources/views/stolpersteine/home.blade.php
+++ resources/views/stolpersteine/home.blade.php
@@ -50,7 +50,7 @@
         });
         
         var map = L.map('map').setView([37.890056, -4.778513], 10);
-        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
         maxZoom: 19
         }).addTo(map);
