70a4825a8aa2a89d719630f66948f6c48b2bf5df
JuanGT01 <jgato@iesflorenciopintado.es>
2022-05-30 21:58:04 +0200

    Social media

--- resources/views/stolpersteine/contact.blade.php
+++ resources/views/stolpersteine/contact.blade.php
@@ -7,19 +7,19 @@
     </div>
     <div class="row justify-content-center w-100 g-0 m-0 p-0">
         <div class="col-2 text-end py-3 px-2"><i class="bi bi-phone opcion-menu"></i></div>
-        <div class="col-10 py-3 px-2 d-flex align-items-center"><button class="btn btn-large btncontacto">+34 123 456 789</button></div>
+        <div class="col-10 py-3 px-2 d-flex align-items-center"><button class="btn btn-large btncontacto"><a href="tel:+34 123 456 789" class="text-decoration-none">+34 123 456 789</a></button></div>
     </div>
     <div class="row justify-content-center w-100 g-0 m-0 p-0">
         <div class="col-2 text-end py-3 px-2"><i class="bi bi-envelope opcion-menu"></i></div>
-        <div class="col-10 py-3 px-2 d-flex align-items-center"><button class="btn btn-large btncontacto">correocontacto@stolpersteine.com</button></div>
+        <div class="col-10 py-3 px-2 d-flex align-items-center"><button class="btn btn-large btncontacto"><a href="mailto:correocontacto@stolpersteine.com" class="text-decoration-none">correocontacto@stolpersteine.com</a></button></div>
     </div>
     <div class="row justify-content-center w-100 g-0 m-0 p-0">
         <div class="col-2 text-end py-3 px-2"><i class="bi bi-facebook opcion-menu"></i></div>
-        <div class="col-10 py-3 px-2 d-flex align-items-center"><button class="btn btn-large btncontacto">Stolpersteine Cordoba</button></div>
+        <div class="col-10 py-3 px-2 d-flex align-items-center"><button class="btn btn-large btncontacto"><a href="https://www.facebook.com/groups/stolpersteinelospedroches/" class="text-decoration-none">Stolpersteine Cordoba</a></button></div>
     </div>
     <div class="row justify-content-center w-100 g-0 m-0 p-0">
         <div class="col-2 text-end py-3 px-2"><i class="bi bi-youtube opcion-menu"></i></div>
-        <div class="col-10 py-3 px-2 d-flex align-items-center"><button class="btn btn-large btncontacto">Stolpersteine Cordoba</button></div>
+        <div class="col-10 py-3 px-2 d-flex align-items-center"><button class="btn btn-large btncontacto"><a href="https://www.youtube.com/channel/UClPnJ5d7ozRYRxFjmREM_HQ" class="text-decoration-none">Stolpersteine Cordoba</a></button></div>
     </div>
 
     <nav class="navbar fixed-bottom navbar-expand navbar-dark p-0" style="background-color: #493620;">
